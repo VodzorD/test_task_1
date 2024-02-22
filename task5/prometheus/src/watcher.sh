82a3887cd9a0bc23920fd1993b1e8ae7a255698f
wscallop <wscallop@doublespeak.ai>
2024-02-22 13:04:58 +0300

    restract

--- task5/prometheus/src/watcher.sh
+++ task5/prometheus/src/watcher.sh
@@ -5,6 +5,4 @@ while inotifywait -e modify ovr.xml; do
         echo "ovr.xml changed. Generating targets..."
     # Run the script to generate targets
         python generator.py
-    # Restart Prometheus container
-        docker-compose restart prometheus
 done
